fe8f81be909239407c290041da73102985ea899f
rehabriad <rehab.riad@socialfruits.com>
2013-01-20 14:39:29 +0200

    ba2a

--- MvcTest/Global.asax.cs
+++ MvcTest/Global.asax.cs
@@ -16,7 +16,7 @@ namespace MvcTest
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : AbstractMvcApplication<Member, GenericRepository<Entity>, Entity>
+    public class MvcApplication :HttpApplication// AbstractMvcApplication<Member, GenericRepository<Entity>, Entity>
     {
 
     }
